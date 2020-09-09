ed3a86c81eeaa88aad26119fb321586c02c0d1d1
Tommy Tao <tommytao1016@gmail.com>
2020-09-09 10:56:12 -0400

    readme

--- container/setenv.sh
+++ container/setenv.sh
@@ -5,10 +5,7 @@ export JAVA_OPTS="$JAVA_OPTS -Ddatabase.user=${DB_USER}"
 export JAVA_OPTS="$JAVA_OPTS -Ddatabase.password=${DB_PASSWORD}"
 export JAVA_OPTS="$JAVA_OPTS -Ddatabase.driver=org.postgresql.Driver"
 export JAVA_OPTS="$JAVA_OPTS -Dkey=test"
+export JAVA_OPTS="$JAVA_OPTS -Dlogging.level.org.springframework=INFO"
 export JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=dev"
+export JAVA_OPTS="$JAVA_OPTS -Dbucketname=tommytao-s3-bucket1"
 export JAVA_OPTS="$JAVA_OPTS -Daws.region=us-east-1"
-export JAVA_OPTS="$JAVA_OPTS -Daws.accessKeyId=AKIAWDMVRNFFYOI2DANQ"
-export JAVA_OPTS="$JAVA_OPTS -Daws.secretKey=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"
-
-
-
